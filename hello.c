084b92891c25f4a777419622ae4a8e6fa5befdb3
7893166871 <62830511+7893166871@users.noreply.github.com>
2023-01-17 11:00:54 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -7,6 +7,7 @@ int main() {
 	char* string = "Anamika Chamola"; // Your name here. ^_^
 	char* string = "Deepak Vishwakarma."; // Your name here. ^_^
 	char* string = "Lanke vamsi kiran."; // Your name here. ^_^
+	char* string = "Bharath Kumar."; // Your name here. ^_^
   char* string = "Sunny Arora."; // Your name here. ^_^
 	char string = "Md. Touhidul Islam"; //Your name here. ^_^
 	printf("%s\n", string);
